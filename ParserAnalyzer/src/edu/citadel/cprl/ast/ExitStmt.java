86f4b6bf46dfcb32ada0615cc70ea3d80585a31c
Fernanda <143466223+Sunref@users.noreply.github.com>
2025-05-15 12:05:08 -0300

    ExitStmt.java done

--- ParserAnalyzer/src/edu/citadel/cprl/ast/ExitStmt.java
+++ ParserAnalyzer/src/edu/citadel/cprl/ast/ExitStmt.java
@@ -3,6 +3,7 @@ package edu.citadel.cprl.ast;
 import edu.citadel.compiler.CodeGenException;
 import edu.citadel.compiler.ConstraintException;
 import edu.citadel.compiler.ErrorHandler;
+import edu.citadel.cprl.LoopContext;
 import edu.citadel.cprl.Type;
 
 /**
@@ -18,7 +19,7 @@ public class ExitStmt extends Statement {
      * should be null if there is no "when" expression) and a reference to the
      * enclosing loop statement.
      */
-    public ExitStmt( Expression whenExpr, LoopStmt loopStmt ) {
+    public ExitStmt(Expression whenExpr, LoopStmt loopStmt) {
         this.whenExpr = whenExpr;
         this.loopStmt = loopStmt;
     }
@@ -30,27 +31,37 @@ public class ExitStmt extends Statement {
     public LoopStmt getLoopStmt() {
         return loopStmt;
     }
-    
+
     @Override
     public void checkConstraints() {
-        
         // Regra de Tipo: se uma expressão when existir, o seu tipo deve ser
         // Boolean.
-        
+
         // Regra Variada: a instrução exit deve estar aninhada dentro de uma
         // instrução de laço, o que é tratado pelo parser usando LoopContext.
-        
-        // <editor-fold defaultstate="collapsed" desc="Implementação">
-                    
-        // sua implementação aqui
 
-        // </editor-fold>
-        
+        // Implementação:
+
+        try {
+            if (whenExpr != null) {
+                whenExpr.checkConstraints();
+                if (!whenExpr.getType().equals(Type.Boolean)) {
+                    String errorMsg = "When expression must be of type Boolean";
+                    throw error(whenExpr.getPosition(), errorMsg);
+                }
+            }
+
+            if (loopStmt == null) {
+                String errorMsg = "Exit statement must be inside a loop";
+                throw error(whenExpr.getPosition(), errorMsg);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void emit() throws CodeGenException {
         // ...
     }
-    
 }
