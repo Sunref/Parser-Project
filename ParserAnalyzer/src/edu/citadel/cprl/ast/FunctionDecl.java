be8595c2a768c0b1da28165da0579a4632c28ecc
Fernanda <143466223+Sunref@users.noreply.github.com>
2025-06-09 11:04:41 -0300

    emit tests

--- ParserAnalyzer/src/edu/citadel/cprl/ast/FunctionDecl.java
+++ ParserAnalyzer/src/edu/citadel/cprl/ast/FunctionDecl.java
@@ -4,7 +4,6 @@ import edu.citadel.compiler.CodeGenException;
 import edu.citadel.compiler.ConstraintException;
 import edu.citadel.compiler.ErrorHandler;
 import edu.citadel.cprl.Token;
-
 import java.util.List;
 
 /**
@@ -15,8 +14,8 @@ public class FunctionDecl extends SubprogramDecl {
     /**
      * Construct a function declaration with its name (an identifier).
      */
-    public FunctionDecl( Token funcId ) {
-        super( funcId );
+    public FunctionDecl(Token funcId) {
+        super(funcId);
     }
 
     /**
@@ -25,18 +24,16 @@ public class FunctionDecl extends SubprogramDecl {
      * parameters have been set.
      */
     public int getRelAddr() {
-        
         int firstParamAddr = 0;
 
         List<ParameterDecl> params = getFormalParams();
-        if ( params.size() > 0 ) {
-            ParameterDecl firstParamDecl = params.get( 0 );
+        if (params.size() > 0) {
+            ParameterDecl firstParamDecl = params.get(0);
             firstParamAddr = firstParamDecl.getRelAddr();
         }
 
         // the location for the return value is above the first parameter
         return firstParamAddr - getType().getSize();
-        
     }
 
     /**
@@ -48,90 +45,78 @@ public class FunctionDecl extends SubprogramDecl {
      * if statement or a loop statement), then the nested statements are also
      * checked for a return statement.
      */
-    private boolean hasReturnStmt( List<Statement> statements ) {
-        
-        for ( Statement stmt : statements ) {
-            
-            if ( stmt instanceof ReturnStmt ) {
+    private boolean hasReturnStmt(List<Statement> statements) {
+        for (Statement stmt : statements) {
+            if (stmt instanceof ReturnStmt) {
                 return true;
-            } else if ( stmt instanceof IfStmt ) {
-                
+            } else if (stmt instanceof IfStmt) {
                 IfStmt ifStmt = (IfStmt) stmt;
-                if ( hasReturnStmt( ifStmt.getThenStmts() ) ) {
+                if (hasReturnStmt(ifStmt.getThenStmts())) {
                     return true;
                 }
 
-                for ( ElsifPart elsifPart : ifStmt.getElsifParts() ) {
-                    if ( hasReturnStmt( elsifPart.getThenStmts() ) ) {
+                for (ElsifPart elsifPart : ifStmt.getElsifParts()) {
+                    if (hasReturnStmt(elsifPart.getThenStmts())) {
                         return true;
                     }
                 }
 
-                if ( hasReturnStmt( ifStmt.getElseStmts() ) ) {
+                if (hasReturnStmt(ifStmt.getElseStmts())) {
                     return true;
                 }
-                
-            } else if ( stmt instanceof LoopStmt ) {
-                
+            } else if (stmt instanceof LoopStmt) {
                 LoopStmt loopStmt = (LoopStmt) stmt;
-                if ( hasReturnStmt( loopStmt.getStatements() ) ) {
+                if (hasReturnStmt(loopStmt.getStatements())) {
                     return true;
                 }
-                
             }
-            
         }
 
         return false;
-        
     }
-    
+
     @Override
     public void checkConstraints() {
-        
         // Regra Variada: não pode haver nenhuma var nos parâmetros.
-        
-        // Regra Variada: é necessário que haja pelo menos uma instrução de 
+
+        // Regra Variada: é necessário que haja pelo menos uma instrução de
         // retorno.
-        
+
         // Dica: veja a implementação de SubprogramDecl
-        
+
         try {
-            
-            for ( InitialDecl decl : getInitialDecls() ) {
+            for (InitialDecl decl : getInitialDecls()) {
                 decl.checkConstraints();
             }
 
-            for ( ParameterDecl paramDecl : getFormalParams() ) {
-                if ( paramDecl.isVarParam() ) {
+            for (ParameterDecl paramDecl : getFormalParams()) {
+                if (paramDecl.isVarParam()) {
                     String errorMsg = "A function cannot have var parameters.";
-                    throw error( paramDecl.getPosition(), errorMsg );
+                    throw error(paramDecl.getPosition(), errorMsg);
                 }
                 paramDecl.checkConstraints();
             }
 
-            if ( !hasReturnStmt( getStatementPart().getStatements() ) ) {
-                String errorMsg = "A function must have at least one return statement.";
-                throw error( getPosition(), errorMsg );
+            if (!hasReturnStmt(getStatementPart().getStatements())) {
+                String errorMsg =
+                    "A function must have at least one return statement.";
+                throw error(getPosition(), errorMsg);
             }
 
             getStatementPart().checkConstraints();
-        
-        } catch ( ConstraintException e ) {
-            ErrorHandler.getInstance().reportError( e );
+        } catch (ConstraintException e) {
+            ErrorHandler.getInstance().reportError(e);
         }
-        
     }
-    
+
     @Override
     public void emit() throws CodeGenException {
-        
-        // <editor-fold defaultstate="collapsed" desc="Implementação">
-                    
-        // sua implementação aqui
+        // Implementação:
+        setRelativeAddresses();
 
-        // </editor-fold>
-        
+        emitLabel(getSubprogramLabel());
+        emit("PROGRAM" + getVarLength());
+        getStatementPart().emit();
+        emit("RET" + getParamLength());
     }
-    
 }
--- ParserAnalyzer/src/edu/citadel/cprl/ast/ProcedureCallStmt.java
+++ ParserAnalyzer/src/edu/citadel/cprl/ast/ProcedureCallStmt.java
@@ -4,7 +4,6 @@ import edu.citadel.compiler.CodeGenException;
 import edu.citadel.compiler.ConstraintException;
 import edu.citadel.compiler.ErrorHandler;
 import edu.citadel.cprl.Token;
-
 import java.util.List;
 
 /**
@@ -21,14 +20,14 @@ public class ProcedureCallStmt extends Statement {
      * list of actual parameters being passed as part of the call,
      * and a reference to the declaration of the procedure being called.
      */
-    public ProcedureCallStmt( Token procId,
-            List<Expression> actualParams,
-            ProcedureDecl procDecl ) {
-        
+    public ProcedureCallStmt(
+        Token procId,
+        List<Expression> actualParams,
+        ProcedureDecl procDecl
+    ) {
         this.procId = procId;
         this.actualParams = actualParams;
         this.procDecl = procDecl;
-        
     }
 
     public Token getProcId() {
@@ -42,68 +41,63 @@ public class ProcedureCallStmt extends Statement {
     public ProcedureDecl getProcDecl() {
         return procDecl;
     }
-    
+
     @Override
     public void checkConstraints() {
-        
         // Regra de Tipo: a quantidade de argumentos (actual parameters)
         // precisa ser a mesma da quantidade de parâmetros formais e cada par
         // deve ter o mesmo tipo.
-        
+
         // Regra Variada: se um parâmetro formal é um parâmetro var, então o
         // argumento deve ser um valor nomeado, não uma expressão arbitrária.
-        
+
         try {
-            
             List<ParameterDecl> formalParams = procDecl.getFormalParams();
 
-            if ( actualParams.size() != formalParams.size() ) {
+            if (actualParams.size() != formalParams.size()) {
                 String errorMsg = "Incorrect number of actual parameters.";
-                throw error( procId.getPosition(), errorMsg );
+                throw error(procId.getPosition(), errorMsg);
             }
 
-            for ( Expression expr : actualParams ) {
+            for (Expression expr : actualParams) {
                 expr.checkConstraints();
             }
 
-            for ( int i = 0; i < actualParams.size(); ++i ) {
-                
-                Expression expr = actualParams.get( i );
-                ParameterDecl param = formalParams.get( i );
+            for (int i = 0; i < actualParams.size(); ++i) {
+                Expression expr = actualParams.get(i);
+                ParameterDecl param = formalParams.get(i);
 
-                if ( !matchTypes( expr.getType(), param.getType() ) ) {
+                if (!matchTypes(expr.getType(), param.getType())) {
                     String errorMsg = "Parameter type mismatch.";
-                    throw error( expr.getPosition(), errorMsg );
+                    throw error(expr.getPosition(), errorMsg);
                 }
 
-                if ( param.isVarParam() ) {
-                    if ( expr instanceof NamedValue ) {
+                if (param.isVarParam()) {
+                    if (expr instanceof NamedValue) {
                         // troca o valor nomeado por uma variável
-                        expr = new Variable( (NamedValue) expr );
-                        actualParams.set( i, expr );
+                        expr = new Variable((NamedValue) expr);
+                        actualParams.set(i, expr);
                     } else {
-                        String errorMsg = "Expression for a var parameter must be a variable.";
-                        throw error( expr.getPosition(), errorMsg );
+                        String errorMsg =
+                            "Expression for a var parameter must be a variable.";
+                        throw error(expr.getPosition(), errorMsg);
                     }
                 }
-                
             }
-            
-        } catch ( ConstraintException e ) {
-            ErrorHandler.getInstance().reportError( e );
+        } catch (ConstraintException e) {
+            ErrorHandler.getInstance().reportError(e);
         }
-        
     }
 
     @Override
     public void emit() throws CodeGenException {
-        
-        // <editor-fold defaultstate="collapsed" desc="Implementação">
-                    
-        // sua implementação aqui
+        // Implementação:
+        emit("ALLOC " + procDecl.getType().getSize());
 
-        // </editor-fold>
-        
+        for (Expression expr : actualParams) {
+            expr.emit();
+        }
+
+        emit("CALL " + procId.getSubprogramLabel());
     }
-    
 }
